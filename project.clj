7b33cb85fd42ba1fa7f5728ab3c9cb7f35e831e5
Kenneth Leung <ccllnekk@gmail.com>
2017-01-12 14:54:35 -0800

    kl

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
 
   :license {:url "http://www.eclipse.org/legal/epl-v10.html"
             :name "Eclipse Public License"}
-  ;;:scm "https://github.com/llnek/lein-czlab.git"
   :url "https://github.com/llnek/lein-czlab"
 
   :description "(hooks) to extend the build process."
